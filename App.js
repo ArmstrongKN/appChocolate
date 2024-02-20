db1ed633f810c8e2fa0de97fb4a8a976c8443d3d
ArmstrongKN <kioshinambu@hotmail.com>
2024-02-19 22:35:28 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>CHOCOLATERIA</Text>
+
+      <ScrollView>
+        <Text style={styles.subtitulo}>Cacau Show</Text>
+        <Image style={styles.img} resizeMode="center" source={require("./assets/cacauShow.jpg")} />
+
+        <Text style={styles.subtitulo}>Kopenhagen</Text>
+        <Image style={styles.img} resizeMode="center" source={require("./assets/Kopenhagen.jpg")} />
+
+        <Text style={styles.subtitulo}>Nuts</Text>
+        <Image style={styles.img} resizeMode="center" source={require("./assets/nuts.jpg")} />
+      </ScrollView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a18262',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  img: {
+    width: 250,
+    height: 200,
+    borderRadius: 8,
+    marginVertical: 15,
+  }
+});
